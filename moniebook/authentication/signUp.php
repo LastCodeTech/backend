956609c6f9400ce7e7df247921096393291bbe56
LastCodeTech <sulaimonabubakre260@gmail.com>
2025-11-01 15:17:16 +0100

    shshhs

---
+++ moniebook/authentication/signUp.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<head>
+    <title></title>
+    <link rel="stylesheet" href="https://unpkg.com/aos@2.3.4/dist/aos.css" />
+  <script src="https://unpkg.com/aos@2.3.4/dist/aos.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+  <script src="https://kit.fontawesome.com/39c5fdd9a0.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class='flex justify-center'>
+        <div>
+            <img src='../images/moniepo'
+    <h1 class="text font-bold text-xl capitalize my-5 text-center">Sign in with Moniepoint</h1>
+    <h2 class='text-base.font-semi-bold text-center'>Sign in with your Moniepoint details to continue</h2>
+    <div>
+        <form method='post' action='post'>
+        <div class='mt-5'><label for='phoneNumber'>phone number</label><br />
+        <input type='tel' id='phoneNumber' name='phoneNumber' class='border-1 h-12 w-100 rounded-xl outline-none'></div>
+        <div class='my-5'>
+            <label for='password'>password</label><br />
+            <input type='password' id='password' name ='password' class='border-1 h-12 w-100 rounded-xl outline-none'>
+        </div>
+        <input type='submit' value='continue' class='h-10 px-42 rounded-xl text-white bg-blue-600 text-base capitalize'>
+        </form>
+    </div>
+</div>
+</div>
+</body>
+</html>
